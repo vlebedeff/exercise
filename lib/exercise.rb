d14e8d23d6bae2498090f75c3bdf3c76f34146d1
Vlad Lebedev <vladlebedeff@gmail.com>
2018-05-19 15:25:28 +0300

    Implement gcd

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -8,4 +8,11 @@ module Exercise
     return 1 if position == 1
     (1..position).reduce([0, 1]) { |(prev, current), _| [current, (prev + current) % 10] }.first
   end
+
+  def gcd(a, b) # rubocop:disable Naming/UncommunicativeMethodParamName
+    return a if b.zero?
+    return b if a.zero?
+    a, b = a > b ? [b, a - b] : [a, b - a] while a != b && a > 1
+    a
+  end
 end
--- spec/lib/exercise_spec.rb
+++ spec/lib/exercise_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Exercise do
       expect(fib_mod_10(327_305)).to eq(5)
     end
   end
+
+  describe '#gcd' do
+    it 'calculates greatest common divisor of two numbers' do
+      expect(gcd(18, 35)).to eq(1)
+      expect(gcd(28_851_538, 1_183_019)).to eq(17_657)
+      expect(gcd(1, 2_000_000_000)).to eq(1)
+    end
+  end
 end

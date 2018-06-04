86b5d08ab0d65d5ca0092db41075d1bd04933eb9
Vlad Lebedev <vladlebedeff@gmail.com>
2018-06-04 10:04:05 +0300

    Implement fractional knapsack

--- lib/exercise/fractional_knapsack.rb
+++ lib/exercise/fractional_knapsack.rb
@@ -4,6 +4,15 @@ module Exercise
   # @param items [Array<Array(Integer, Integer)>] list of items represented with pairs of value and weight
   # @return value [Number] maximum value for the given capacity
   def fractional_knapsack(capacity, items)
-    180
+    sorted_items = items.sort_by { |value, weight| weight.to_f / value }
+    target_value = 0
+    left_capacity = capacity
+    while left_capacity > 0 && sorted_items.any?
+      value, weight = sorted_items.shift
+      item_fraction = [1, Rational(left_capacity, weight)].min
+      target_value += item_fraction * value
+      left_capacity -= item_fraction * weight
+    end
+    target_value.to_f.round(4)
   end
 end
--- spec/lib/exercise/fractional_knapsack_spec.rb
+++ spec/lib/exercise/fractional_knapsack_spec.rb
@@ -5,6 +5,7 @@ module Exercise
     specify do
       expect(fractional_knapsack(50, [[60, 20], [100, 50], [120, 30]])).to eq(180.0)
       expect(fractional_knapsack(10, [[500, 30]])).to eq(166.6667)
+      expect(fractional_knapsack(1000, [[500, 30]])).to eq(500.0)
     end
   end
 end

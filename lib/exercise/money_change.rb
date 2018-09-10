c6512d1ee4423de67cc49a0e20f8f1f1dd727875
Vlad Lebedev <vladlebedeff@gmail.com>
2018-09-10 09:46:46 +0300

    Adjust `#money_change` for arbitrary denominations

--- lib/exercise/money_change.rb
+++ lib/exercise/money_change.rb
@@ -1,16 +1,20 @@
 module Exercise
-  COIN_DENOMINATIONS = [10, 5, 1].freeze
   # Calculates the minimum number of coins needed to change the given amount of money
   #
   # @param amount [Integer] the amount of money
   # @return [Integer] the number of coins
-  def money_change(amount)
-    number_of_coins = 0
-    while amount > 0
-      coin = COIN_DENOMINATIONS.find { |d| d <= amount }
-      number_of_coins += amount / coin
-      amount = amount % coin
+  def money_change(amount, coin_denominations = [1])
+    denominations_desc = coin_denominations.sort_by(&:-@)
+    min_coins = Array.new(amount + 1) { Float::INFINITY }
+    1.upto(amount) do |a|
+      denominations_desc.each do |d|
+        if a == d
+          min_coins[a] = 1
+        elsif a > d
+          min_coins[a] = [min_coins[a], 1 + min_coins[a - d]].min
+        end
+      end
     end
-    number_of_coins
+    min_coins.last
   end
 end
--- spec/lib/exercise/money_change_spec.rb
+++ spec/lib/exercise/money_change_spec.rb
@@ -2,9 +2,20 @@ module Exercise
   RSpec.describe '#money_change' do
     include Exercise
 
+    let(:coin_denominations) { [10, 5, 1] }
+
+    let(:examples) do
+      [
+        [2,  [10, 5, 1], 2],
+        [28, [10, 5, 1], 6],
+        [40, [25, 20, 10, 5, 1], 2]
+      ]
+    end
+
     specify do
-      expect(money_change(2)).to eq(2)
-      expect(money_change(28)).to eq(6)
+      examples.each do |money, denominations, num_coins|
+        expect(money_change(money, denominations)).to eq(num_coins)
+      end
     end
   end
 end

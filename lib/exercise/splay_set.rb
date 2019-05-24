342f097549de7c94e743910259e3c85a393bac46
Vlad Lebedev <vladlebedeff@gmail.com>
2019-05-24 07:57:48 +0300

    Test splay set on input of size 2

--- lib/exercise/splay_set.rb
+++ lib/exercise/splay_set.rb
@@ -1,8 +1,8 @@
 module Exercise
   class SplaySet
-    attr_reader :node
+    attr_reader :node, :path
 
-    def initialize(node: nil, path: Stack.new)
+    def initialize(node: nil, path: Stack.new(nil, nil, 0))
       @node = node
       @path = path
     end
@@ -11,17 +11,30 @@ module Exercise
       node.nil?
     end
 
+    def insert(value)
+      return SplaySet.new(node: Node.new(value)) if empty?
+      set = find(value)
+      return set.splay if set.node.value == value
+      new_node = Node.new(value)
+      if new_node.value > set.node.value
+        SplaySet.new(node: set.node.attach_right(new_node), path: set.path).go_right.splay
+      else
+        SplaySet.new(node: set.node.attach_left(new_node), path: set.path).go_left.splay
+      end
+    end
+
     def go_left
       return self unless node.left
-      SplaySet.new(node.left, path.push(Step.new(node, :left)))
+      SplaySet.new(node: node.left, path: path.push(Step.new(node, :left)))
     end
 
     def go_right
       return self unless node.right
-      SplaySet.new(node.right, path.push(Step.new(node, :right)))
+      SplaySet.new(node: node.right, path: path.push(Step.new(node, :right)))
     end
 
     def find(value)
+      return nil if empty?
       z = self
       loop do
         if z.node.value < value && z.node.right
@@ -34,23 +47,35 @@ module Exercise
       end
     end
 
+    def sum(range)
+      return 0 if empty?
+      splay_left = find(range.begin).splay
+      prune_left = SplaySet.new(node: splay_left.node.prune_left)
+      splay_right = prune_left.find(range.end).splay
+      prune_right = SplaySet.new(node: splay_right.node.prune_right)
+      prune_right.node.sum
+    end
+
     def splay
       z = self
       while z.path.any?
         z =
           if z.path.size == 1
-            if z.last.direction == :left
-              new_right = z.last.node.attach_left(z.node.right)
-              SplaySet.new(node.attach_right(new_right))
+            if z.path.last.direction == :left
+              new_right = z.path.last.node.attach_left(z.node.right)
+              SplaySet.new(node: node.attach_right(new_right))
             else
-              new_left = z.last.node.attach_right(z.node.left)
-              SplaySet.new(node.attach_left(new_left))
+              new_left = z.path.last.node.attach_right(z.node.left)
+              SplaySet.new(node: node.attach_left(new_left))
             end
           else
             z
           end
       end
+      z
     end
+
+    ###########################################
 
     Node = Struct.new(:value, :left, :right) do
       def attach_left(node)
@@ -59,6 +84,14 @@ module Exercise
 
       def attach_right(node)
         Node.new(value, left, node)
+      end
+
+      def prune_left
+        Node.new(value, nil, right)
+      end
+
+      def prune_right
+        Node.new(value, left, nil)
       end
 
       def sum
@@ -71,12 +104,9 @@ module Exercise
       end
     end
 
-    Stack = Struct.new(:value, :prev, :size) do
-      def initalize(*args)
-        super
-        self.size ||= 0
-      end
+    ###########################################
 
+    Stack = Struct.new(:value, :prev, :size) do
       def push(value)
         Stack.new(value, self, size + 1)
       end
@@ -93,6 +123,8 @@ module Exercise
         !value.nil?
       end
     end
+
+    ###########################################
 
     Step = Struct.new(:node, :direction)
   end
--- spec/lib/exercise/splay_set_spec.rb
+++ spec/lib/exercise/splay_set_spec.rb
@@ -1,4 +1,35 @@
 RSpec.describe Exercise::SplaySet do
+  shared_examples 'splay tree set' do |size|
+    context 'when values are inserted in ascending order' do
+      let(:values) { Array.new(size) { |i| i } }
+      let(:baseline_set) { Set.new(values) }
+
+      subject(:splay_set) do
+        values.reduce(described_class.new) do |set, value|
+          set.insert(value)
+        end
+      end
+
+      describe '#find' do
+        specify do
+          values_found = values.map { |v| splay_set.find(v).node.value }
+          expect(values_found).to eq(values)
+        end
+      end
+
+      describe '#sum' do
+        let(:bounds) { Range.new(*values.shuffle.first(2).sort) }
+        let(:baseline_sum) { baseline_set.reduce(0) { |sum, v| bounds.cover?(v) ? sum + v : v } }
+
+        specify do
+          expect(splay_set.sum(bounds)).to eq(baseline_sum)
+        end
+      end
+    end
+  end
+
+  it_behaves_like 'splay tree set', 2
+
   describe '#empty?' do
     subject { described_class.new.empty? }
 
@@ -11,5 +42,13 @@ RSpec.describe Exercise::SplaySet do
 
       it { is_expected.to eq(false) }
     end
+  end
+
+  describe '#insert' do
+    subject { described_class.new.insert(0) }
+
+    it { is_expected.not_to be_empty }
+
+    specify { expect(subject.node.value).to eq(0) }
   end
 end

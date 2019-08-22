4d0308f7ed5add70ce739c19c93eec8ee528cffa
Vlad Lebedev <vladlebedeff@gmail.com>
2019-08-22 09:29:07 +0300

    Document spec/lib/exercise/balanced_partition_spec.rbbalanced_partiotion3

--- lib/exercise/balanced_partition.rb
+++ lib/exercise/balanced_partition.rb
@@ -1,12 +1,15 @@
 module Exercise
+  # Determines whether a set of elements can be partitioned in 3 subsets such that the sum of
+  # elements in all 3 subsets is the same
   def balanced_partition3(items)
     sum_third, sum_mod3 = items.reduce(:+).divmod(3)
     return false unless sum_mod3.zero?
-    table = Array.new(sum_third + 1) do |i|
-      Array.new(sum_third + 1) do |j|
-        Array.new(items.size + 1) { i.zero? || j.zero? ? true : false }
+    table =
+      Array.new(sum_third + 1) do |i|
+        Array.new(sum_third + 1) do |j|
+          Array.new(items.size + 1) { i.zero? || j.zero? ? true : false }
+        end
       end
-    end
 
     1.upto(sum_third) do |i|
       1.upto(sum_third) do |j|
